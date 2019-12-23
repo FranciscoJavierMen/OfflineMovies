e01677a9276c7a427f70dc1e13504aedf8a14297
Javi_Altatec <francisco.javier.mendoza.licea@gmail.com>
2019-12-23 14:47:00 -0600

    Agregar el RequestInterceptor para agregar api_key a la cabecera de la petición

--- src/main/java/com/example/offlinemovies/data/MovieRepository.java
+++ src/main/java/com/example/offlinemovies/data/MovieRepository.java
@@ -2,7 +2,9 @@ package com.example.offlinemovies.data;
 
 import com.example.offlinemovies.data.remote.ApiConstants;
 import com.example.offlinemovies.data.remote.MovieAPIService;
+import com.example.offlinemovies.data.remote.RequestInterceptor;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,9 +13,16 @@ public class MovieRepository {
     private MovieAPIService service;
 
     public MovieRepository() {
+        //RequestInterceptor para incluir en la cabecera de la petición
+        //el api_key
+        OkHttpClient.Builder okhttpClientBuilder = new OkHttpClient.Builder();
+        okhttpClientBuilder.addInterceptor(new RequestInterceptor());
+        OkHttpClient client = okhttpClientBuilder.build();
+
         //Remote > Retrofit
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(ApiConstants.BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
