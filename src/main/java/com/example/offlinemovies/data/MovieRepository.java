7639de2a6bb6597d0c3c74435cb907fc983e2f22
Javi_Altatec <francisco.javier.mendoza.licea@gmail.com>
2019-12-23 15:26:54 -0600

    Creación de lógica para obtener peliculas desde API externa o desde base de datos local

--- src/main/java/com/example/offlinemovies/data/MovieRepository.java
+++ src/main/java/com/example/offlinemovies/data/MovieRepository.java
@@ -1,15 +1,24 @@
 package com.example.offlinemovies.data;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.room.Room;
 
 import com.example.offlinemovies.app.MyApp;
 import com.example.offlinemovies.data.local.MovieRoomDatabase;
 import com.example.offlinemovies.data.local.dao.MovieDao;
+import com.example.offlinemovies.data.local.entity.MovieEntity;
+import com.example.offlinemovies.data.network.NetworkBoundResource;
+import com.example.offlinemovies.data.network.Resource;
 import com.example.offlinemovies.data.remote.ApiConstants;
 import com.example.offlinemovies.data.remote.MovieAPIService;
 import com.example.offlinemovies.data.remote.RequestInterceptor;
+import com.example.offlinemovies.data.remote.model.MoviesResponse;
+
+import java.util.List;
 
 import okhttp3.OkHttpClient;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -43,5 +52,30 @@ public class MovieRepository {
                 .build();
 
         service = retrofit.create(MovieAPIService.class);
+    }
+
+    public LiveData<Resource<List<MovieEntity>>> getPopularMovies(){
+        //Tipo que devuelve Room(Local) y tipo que devuelve el API(remoto) con Retrofit
+        return new NetworkBoundResource<List<MovieEntity>, MoviesResponse>(){
+
+            @Override
+            protected void saveCallResult(@NonNull MoviesResponse item) {
+                movieDao.saveMovies(item.getResults());
+            }
+
+            @NonNull
+            @Override
+            protected LiveData<List<MovieEntity>> loadFromDb() {
+                //Retorna los datos de la base de dato local con Room
+                return movieDao.loadMovies();
+            }
+
+            @NonNull
+            @Override
+            protected Call<MoviesResponse> createCall() {
+                //Realiza peticion a API remota
+                return service.loadPopularMovies();
+            }
+        }.getAsLiveData();
     }
 }
